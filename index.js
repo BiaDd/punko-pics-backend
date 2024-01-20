b40c0a514bb8b22509736ae21b78e865483ec55d
Dan Do <dxd3vmb@virginia.edu>
2024-01-19 22:36:37 -0500

    Initialize express stuff

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config;
+const express = require('express');
+const cors = require("cors");
+const router = require('./src/router');
+const app = express();
+const port = process.env.PORT || 8000;
+
+
+app.use(cors())
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+})
+
+app.use("/", router);
