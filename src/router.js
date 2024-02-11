418e7253d2dba20018ba25840813fd8c562986e1
Dan Do <dxd3vmb@virginia.edu>
2024-02-10 23:18:43 -0500

    oops forgot to add back api key check

--- src/router.js
+++ src/router.js
@@ -10,23 +10,30 @@ router.get('/', (req, res) => {
 
 // Get all the keys from bucket
 router.get('/pictures', async (req, res) => {
-    let result = await getAllFiles();
-    if (result) {
-        const keySize = result.length
-        let contents = result.sort((a, b) => b.metadata.lastModified - a.metadata.lastModified)
-        let pics = []
-        for (let i = 0; i < keySize; i++) {
-            pics.push({
-                key: contents[i].name,
-                url: await getFileUrl(contents[i].name),
-                date: contents[i].metadata.lastModified
+    if (req.headers['punkopics-api-key'] === process.env.PUNKOPICS_API_KEY || req.headers['punkopics-api-key'] === dotenv.parsed.PUNKOPICS_API_KEY) {
+        let result = await getAllFiles();
+        if (result) {
+            const keySize = result.length
+            let contents = result.sort((a, b) => b.metadata.lastModified - a.metadata.lastModified)
+            let pics = []
+            for (let i = 0; i < keySize; i++) {
+                pics.push({
+                    key: contents[i].name,
+                    url: await getFileUrl(contents[i].name),
+                    date: contents[i].metadata.lastModified
+                })
+            }
+            res.status(200).json({
+                status: "Success",
+                size: keySize,
+                pictures: pics
             })
         }
-        res.status(200).json({
-            status: "Success",
-            size: keySize,
-            pictures: pics
-        })
+        else {
+            res.status(200).json({
+                status: "Failed"
+            })
+        }
     }
     else {
         res.status(200).json({
